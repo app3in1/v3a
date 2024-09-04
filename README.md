7e3f7d7df868f0526beaba39f25ddf71bc0354f9
ivanooi <ivanooi@localhost>
2024-09-04 22:55:36 +0800

    Commit message goes here

---
+++ README.md
@@ -0,0 +1,9 @@
+# V3a
+
+{{ appDescription }}
+
+[Releases](https://github.com/app3in1/v3a/releases)
+
+## About this Project
+
+This project was generated using the Swing project template from [jDeploy](https://www.jdeploy.com), a tool to deploy Java desktop apps on Windows, Mac.
---
+++ src/test/java/com/github/app3in1/v3a/V3aTest.java
@@ -0,0 +1,38 @@
+package com.github.app3in1.v3a;
+
+import junit.framework.Test;
+import junit.framework.TestCase;
+import junit.framework.TestSuite;
+
+/**
+ * Unit test for simple App.
+ */
+public class V3aTest
+    extends TestCase
+{
+    /**
+     * Create the test case
+     *
+     * @param testName name of the test case
+     */
+    public V3aTest( String testName )
+    {
+        super( testName );
+    }
+
+    /**
+     * @return the suite of tests being tested
+     */
+    public static Test suite()
+    {
+        return new TestSuite( V3aTest.class);
+    }
+
+    /**
+     * Rigourous Test :-)
+     */
+    public void testApp()
+    {
+        assertTrue( true );
+    }
+}
